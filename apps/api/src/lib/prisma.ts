aaacfbd58a2d970d6f53d83539e3ba94ee52beaf
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-04-27 08:53:18 +0530

    feat(prisma): added singleton prisma client export

---
+++ apps/api/src/lib/prisma.ts
@@ -0,0 +1,27 @@
+import { env } from "@/config/env";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client/extension";
+import pg from "pg";
+
+
+const  connectionString = env.DATABASE_URL;
+
+const adapter = new PrismaPg(new pg.Pool({
+    connectionString
+}));
+
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+    adapter,
+    log: env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+});
+
+
+if (env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
