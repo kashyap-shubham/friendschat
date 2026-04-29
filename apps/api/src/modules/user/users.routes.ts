767d771a723ac95237e9555c39526030ce534342
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-04-29 19:26:34 +0530

    feat(user): connected user routes to controller

--- apps/api/src/modules/user/users.routes.ts
+++ apps/api/src/modules/user/users.routes.ts
@@ -1,3 +1,16 @@
+import { Router } from "express";
+import { UserController } from "./users.controller";
 
 
 
+const userRouter: Router = Router();
+const userController = new UserController();
+
+
+userRouter.get("/me", userController.getCurrentUser);
+
+
+userRouter.patch("/me", userController.updateCurrentUser);
+
+
+export default userRouter;
