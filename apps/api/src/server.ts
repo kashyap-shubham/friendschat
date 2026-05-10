396eb6104e48daab8cda6a6069988d876aec5177
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-05-11 01:03:14 +0530

    feat(server): added sever logic

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,19 +1,17 @@
 import express, { type Application } from "express";
 import cors from "cors";
 import session from "express-session";
-import passport from "passport";
+import passport from "@/auth/passport";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import compression from "compression";
 
-import routes from "./routes"
+import routes from "./routes";
 import { env } from "./config/env";
 import { errorHandler } from "./errors/errorHandler";
 import { httpLogger } from "@repo/logger";
 
-
 const app: Application = express();
-
 
 /* Trust Proxy
 | Required when deploying behind reverse proxy (nginx, vercel, railway, etc.)
@@ -21,31 +19,41 @@ const app: Application = express();
 */
 app.set("trust proxy", 1);
 
-
 /* Security Headers
 | Adds HTTP security headers to protect against:
 | XSS, clickjacking, MIME sniffing, etc.
 */
 app.use(helmet());
 
-
 /* CORS Configuration
 | Allows frontend to communicate with backend
 | credentials:true required for cookies (session auth)
 */
+const allowedOrigins =
+  env.NODE_ENV === "production"
+    ? ["https://friendschat.space"]
+    : ["http://localhost:3000"];
+
 app.use(
   cors({
-    origin: env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+
+    },
+
     credentials: true,
   }),
 );
-
 
 /* Compression
 | Compresses HTTP responses (gzip) to improve performance
 */
 app.use(compression());
-
 
 /* Request Logging
 | Logs incoming requests (method, url, status)
@@ -53,8 +61,10 @@ app.use(compression());
 */
 app.use(httpLogger);  // Todo: right now is printed on the screen but in production change the strategy to file or cloud
 
-
-// body parser
+/* Body Parsers
+| Parses incoming request body
+| Limit size to prevent large payload abuse
+*/
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -75,7 +85,6 @@ if (env.NODE_ENV === "production") {
 
 }
 
-
 /* Session Configuration
 | Required for Passport Google OAuth
 | Stores user session securely in cookies
@@ -83,15 +92,32 @@ if (env.NODE_ENV === "production") {
 app.use(
   session({
     name: "connect.sid",
+
     secret: env.SESSION_SECRET!,
+
     resave: false,
     saveUninitialized: false,
+
     rolling: true,
+
+    proxy: env.NODE_ENV === "production",
+
     cookie: {
-      httpOnly: true, // prevents JS access to cookie
-      secure: env.NODE_ENV === "production", // HTTPS only in production
-      sameSite: "lax", // protects against CSRF
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7days
+      domain:
+        env.NODE_ENV === "production"
+          ? ".musiconnect.space"
+          : undefined,
+
+      httpOnly: true,
+
+      secure: env.NODE_ENV === "production",
+
+      sameSite:
+        env.NODE_ENV === "production"
+          ? "none"
+          : "lax",
+
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   }),
 );
@@ -102,10 +128,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// api routes
+/* API Routes
+| All application routes prefixed with /api
+*/
 app.use("/api", routes);
-
 
 /* Global Error Handler
 | Handles all thrown errors in one place
